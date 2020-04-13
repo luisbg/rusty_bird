ff7608be64dc23dc3b1c4ffbdc0680ecea0058d0
Luis de Bethencourt <luis@debethencourt.com>
2020-04-13 19:00:05 -0400

    Depend on ggez

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use ggez::*;
+
+struct State {}
+
+impl ggez::event::EventHandler for State {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        Ok(())
+    }
+
+    fn draw(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        Ok(())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Rusty Bird");
+    let state = &mut State {};
+
+    let mut conf = conf::Conf::new();
+    let win_setup = conf::WindowSetup {
+        title: "Rusty Bird".to_owned(),
+        samples: conf::NumSamples::Zero,
+        vsync: true,
+        icon: "".to_owned(),
+        srgb: true,
+    };
+    conf.window_setup = win_setup;
+    conf.window_mode.height = 720.0;
+    conf.window_mode.width = 1280.0;
+
+    let (ref mut ctx, ref mut event_loop) = ContextBuilder::new("rusty_bird", "Luis de Bethencourt")
+        .conf(conf)
+        .build()
+        .unwrap();
+
+    event::run(ctx, event_loop, state).unwrap();
 }
