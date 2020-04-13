22f1336fcc90c933b2548171dcdeac423f58521c
Luis de Bethencourt <luis@debethencourt.com>
2020-04-13 19:00:17 -0400

    Animate the bird's appearance

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ struct State {
     specs_world: World,
     player_input: Direction,
     movement_system: MovementSystem,
+    animation_system: AnimationSystem,
 }
 
 #[derive(Component, Debug, PartialEq)]
@@ -33,6 +34,35 @@ impl Direction {
     }
 }
 
+#[derive(Component, Default, Debug)]
+#[storage(VecStorage)]
+struct Animation {
+    pub current_frame: u32,
+    max: u32,
+    pub images: Vec<graphics::Image>,
+}
+
+impl Animation {
+    fn new(max: u32, images: Vec<graphics::Image>) -> Self {
+        Animation {
+            current_frame: 0,
+            max,
+            images,
+        }
+    }
+
+    fn from_frames(ctx: &mut Context, frames: u32, base_path: &str) -> Self {
+        let mut character_anim = Vec::new();
+
+        for n in 1..frames + 1 {
+            let path = format!("{}{}.png", base_path, n);
+            character_anim.push(graphics::Image::new(ctx, path).unwrap());
+        }
+
+        Animation::new(frames, character_anim)
+    }
+}
+
 struct MovementSystem;
 impl<'a> System<'a> for MovementSystem {
     type SystemData = (Read<'a, Direction>, WriteStorage<'a, Position>);
@@ -48,8 +78,30 @@ impl<'a> System<'a> for MovementSystem {
     }
 }
 
+struct AnimationSystem;
+impl<'a> System<'a> for AnimationSystem {
+    type SystemData = (WriteStorage<'a, Animation>, ReadStorage<'a, Image>);
+
+    fn run(&mut self, data: Self::SystemData) {
+        let (mut anim, _img) = data;
+
+        for anim in (&mut anim).join() {
+            anim.current_frame += 1;
+            if anim.current_frame >= anim.max {
+                anim.current_frame = 0;
+            }
+        }
+    }
+}
+
 impl ggez::event::EventHandler for State {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        const ANIMATION_DESIRED_FPS: u32 = 15;
+
+        while timer::check_update_time(ctx, ANIMATION_DESIRED_FPS) {
+            self.animation_system.run_now(&self.specs_world);
+        }
+
         self.movement_system.run_now(&self.specs_world);
 
         Ok(())
@@ -59,11 +111,21 @@ impl ggez::event::EventHandler for State {
         graphics::clear(ctx, graphics::BLACK);
         let positions = self.specs_world.read_storage::<Position>();
         let images = self.specs_world.read_storage::<Image>();
+        let animations = self.specs_world.read_storage::<Animation>();
 
         for (p, i) in (&positions, &images).join() {
             graphics::draw(
                 ctx,
                 &*i.image,
+                graphics::DrawParam::default().dest(p.position),
+            )
+            .unwrap_or_else(|err| println!("draw error {:?}", err));
+        }
+
+        for (p, a) in (&positions, &animations).join() {
+            graphics::draw(
+                ctx,
+                &(*a).images[(*a).current_frame as usize].clone(),
                 graphics::DrawParam::default().dest(p.position),
             )
             .unwrap_or_else(|err| println!("draw error {:?}", err));
@@ -132,21 +194,15 @@ fn main() {
     let mut world = World::new();
     world.register::<Position>();
     world.register::<Image>();
+    world.register::<Animation>();
 
-    let char_image = match graphics::Image::new(ctx, "/player1.png") {
-        Ok(img) => img,
-        Err(e) => {
-            panic!("Error: {}", e);
-        }
-    };
-    let character = Arc::new(char_image);
-
+    // The bird
     world
         .create_entity()
         .with(Position {
             position: nalgebra::Point2::new(100.0, 200.0),
         })
-        .with(Image { image: character })
+        .with(Animation::from_frames(ctx, 4, "/player"))
         .build();
 
     let player_input = Direction::new();
@@ -154,11 +210,13 @@ fn main() {
     world.insert(player_input_world);
 
     let update_pos = MovementSystem;
+    let update_animation = AnimationSystem;
 
     let state = &mut State {
         specs_world: world,
         player_input,
         movement_system: update_pos,
+        animation_system: update_animation,
     };
 
     event::run(ctx, event_loop, state).unwrap();
